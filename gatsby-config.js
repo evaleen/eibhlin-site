2e5677f599f78df1f927263a640bd7862615a61a
Eibhlin <eibhlin@blstr.co>
2019-10-27 16:18:25 +1100

    aet up contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,5 +45,12 @@ module.exports = {
         src: path.join(__dirname, 'src'),
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
---
+++ gatsby-node.js
@@ -0,0 +1,67 @@
+// const path = require('path')
+
+exports.sourceNodes = ({ actions }) => {
+  const { createTypes } = actions
+
+  createTypes(
+    `
+    type ContentfulIntro implements Node {
+      name: String
+      image: ContentfulAsset
+      copy: contentfulIntroCopyRichTextNode
+    }
+
+    type ContentfulExtras implements Node @infer {
+      name: String
+    }
+
+    type ContentfulVideoEca implements Node {
+      id: String
+      startTime: Int
+      description: contentfulVideoEcaDescriptionTextNode
+    }
+
+    type ContentfulImageEca implements Node {
+      image: ContentfulAsset
+      description: contentfulImageEcaDescriptionTextNode
+    }
+
+    type ContentfulExperience implements Node {
+      name: String
+      description: contentfulExperienceDescriptionTextNode
+      content: [ContentfulWorkExperience]
+    }
+
+    type ContentfulWorkExperience implements Node {
+      name: String
+      image: ContentfulAsset
+      description: contentfulWorkExperienceDescriptionTextNode
+    }
+
+    type ContentfulContact implements Node {
+      name: String
+      description: contentfulContactDescriptionTextNode
+      image: ContentfulAsset
+      copy: contentfulContactCopyRichTextNode
+    }
+
+    type ContentfulSocialMedia implements Node {
+      name: String
+      linkedIn: String
+      gitHub: String
+      instagram: String
+    }
+  `
+  )
+}
+
+// TODO
+// exports.onCreateWebpackConfig = ({ actions }) => {
+//   actions.setWebpackConfig({
+//     resolve: {
+//       alias: {
+//         website: path.resolve(__dirname, 'src')
+//       }
+//     }
+//   })
+// }
